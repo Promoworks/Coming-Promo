3b5e63daa18fe096a55b55b221ffb78f628c64f4
Promoworks <35553040+Promoworks@users.noreply.github.com>
2018-01-18 10:43:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var config = require('./config.js');
 
 
 var routes = require('./routes/index');
