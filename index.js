d3e12389515b1257f898bdeed73b0935f617c877
Promoworks <35553040+Promoworks@users.noreply.github.com>
2018-01-18 10:42:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -76,9 +76,7 @@ app.use('/', routes);
 
 
 
-// Set Port
-app.set('port', (process.env.PORT || 8080));
-
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+// Start Server
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
 });
